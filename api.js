8ee5cb1c41635e7954e91d70ddc683c0e337bf87
Joshua Oke <joshua.oke@dipolediamond.com>
2019-08-14 15:44:49 +0100

    js and css and html up

---
+++ api.js
@@ -0,0 +1,50 @@
+//function to fetch account details
+function fetchAccount() {
+  const accountNumber = document.getElementById("accountNumber").value;
+  //url for the service
+  const url = "https://nodered.dipolediamond.com/account/" + accountNumber;
+  axios
+    .get(url)
+    .then(function(response) {
+      if (response.status == 200) {
+        //if status is 200, populate values for name and account type
+        document.getElementById("fullName").value = response.data.name;
+        document.getElementById("accountType").value =
+          response.data.account_type;
+
+        //if no email, alert else put value
+        if (response.data.email == "") {
+          alert("Account has no email to receive statement");
+        } else {
+          document.getElementById("email").value = response.data.email;
+        }
+        //account number does not exist
+      } else {
+        alert("Sorry! Incorrect Account Number");
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+//function to check and alert ok
+function checkSubmit() {
+  let name = document.getElementById("fullName").value;
+  let account_type = document.getElementById("accountType").value;
+  let email = document.getElementById("email").value;
+  let endDate = document.getElementById("endDate").value;
+  let startDate = document.getElementById("startDate").value;
+
+  if (
+    name != "" &&
+    account_type != "" &&
+    email != "" &&
+    endDate != "" &&
+    startDate != ""
+  ) {
+    alert("OK!");
+  } else {
+    alert("Some Parameters are missing");
+  }
+}
